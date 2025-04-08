01e5afb7b31e2ec465d4c24e5542d5d285ea2272
cmsrwood <dilanfantas@gmail.com>
2025-04-08 15:23:11 -0500

    origenes multiples

--- src/app.js
+++ src/app.js
@@ -5,24 +5,31 @@ const errorHandler = require('./middlewares/errorHandler');
 const requestLogger = require('./middlewares/requestLogger');
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./config/swaggerConfig");
-const { BadRequestError } = require('./errors/ExceptionErrors');
 
-const FRONTEND_URL = process.env.FRONTEND_URL || '*';
+const FRONTEND_URL = process.env.FRONTEND_URL
+    ? process.env.FRONTEND_URL.split(',')
+    : ['http://localhost:5173', 'http://localhost:8081'];
 
 const createApp = () => {
-
     const app = express();
-    // Middlewares
 
+    // Middlewares
     app.use(cors({
-        origin: FRONTEND_URL,
+        origin: (origin, callback) => {
+            if (!origin || FRONTEND_URL.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error('No autorizado por CORS'));
+            }
+        },
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true,
         allowedHeaders: ['Content-Type', 'Authorization'],
     }));
+
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-    app.use(requestLogger)
+    app.use(requestLogger);
 
     // Rutas
     app.use('/api', routes);
@@ -35,10 +42,9 @@ const createApp = () => {
     });
 
     // Manejo de errores global
-
     app.use(errorHandler);
 
     return app;
-}
+};
 
 module.exports = createApp;
