7abcfeb777e53ff359ae4c87bbc04ea6a904524c
Abjura <107774277+Abjuraa@users.noreply.github.com>
2025-05-15 18:18:37 -0500

    env
    arreglo de nombres en el .env de la base de datos del sequelize

--- src/database/database.js
+++ src/database/database.js
@@ -2,10 +2,10 @@ const { Sequelize } = require('sequelize')
 
 const sequelizeDB = new Sequelize(
     process.env.DB_NAME || 'mrhomero',
-    process.env.DB_USER || 'postgres',
+    process.env.DB_USER_SEQUELIZE || 'postgres',
     process.env.DB_PASSWORD || 'admin',
     {
-        port: process.env.DB_PORT || 5432,
+        port: process.env.DB_PORT_SEQUELIZE || 5432,
         host: process.env.DB_HOST || 'localhost',
         dialect: 'postgres',
         logging: false
